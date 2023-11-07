152ee4d70f1f77eaae65183daae3182ae9ab2599
Tapan Kumer Das <voboghure.dev@gmail.com>
2023-11-08 02:50:28 +0600

    first commit

---
+++ admin/src/App.js
@@ -0,0 +1,12 @@
+import './assets/css/styles.css';
+
+function App() {
+    return (
+        <div>
+            <div>Hello</div>
+            <div>This is a paragraph</div>
+        </div>
+    );
+}
+
+export default App;
